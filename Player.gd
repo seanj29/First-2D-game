5f24a99f83ea09210341fcab6d7ca046adb36c82
seanj29 <seanotto29@gmail.com>
2023-10-05 22:36:08 +0100

    Finished player + started mob

--- Player.gd
+++ Player.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 func _process(delta):
@@ -27,3 +30,22 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+func _on_Player_body_entered(body:Node):
+	hide()
+	emit_signal("hit")
+
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
