061529b8b4759026fc55a1ae13d244ea11d0d823
seanj29 <seanotto29@gmail.com>
2023-10-08 16:43:00 +0100

    changed the gitignore + removed newgame

--- Main.gd
+++ Main.gd
@@ -9,7 +9,7 @@ var score
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	new_game()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
